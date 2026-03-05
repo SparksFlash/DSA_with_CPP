53779af28c73aeb2e92649a2ca66860f7660ce2b
SparksFlash <ug2102056@cse.pstu.ac.bd>
2026-03-05 15:47:52 +0600

    2D-arrays

---
+++ 2D-Arrays/spiralMatrix.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void spiralMatrix(int arr[][4], int n, int m){
+    //top
+
+    //right
+
+    //bottom
+
+    //left
+}
+
+int main(){
+    int arr[4][4] = {{1, 2, 3, 4},
+                     {5, 6, 7, 8},
+                     {9, 10, 11, 12},
+                     {13, 14, 15, 16}};
+    
+}

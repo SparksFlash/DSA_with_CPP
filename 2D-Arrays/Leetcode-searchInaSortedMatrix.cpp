5ca50ceb9d2d94a7b338129bfd078cd4244dec2e
SparksFlash <ug2102056@cse.pstu.ac.bd>
2026-03-07 20:46:53 +0600

    searcInaSortedMatrix leetcode solved

---
+++ 2D-Arrays/Leetcode-searchInaSortedMatrix.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int start = 0, end = matrix[0].size() - 1;
+
+        for(int i = 0; i < n; i++){
+            start = 0;
+            end = matrix[0].size() - 1;
+
+            while(start <= end){
+                int mid = (start + end) / 2;
+
+                if(matrix[i][mid] == target)
+                    return true;
+                else if(matrix[i][mid] > target)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<vector<int>> matrix = {
+        {1,3,5,7},
+        {10,11,16,20},
+        {23,30,34,60}
+    };
+
+    int target = 3;
+    if(obj.searchMatrix(matrix, target))
+        cout << "Found";
+    else
+        cout << "Not Found";
+}

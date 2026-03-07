483d208e1d53bd670eb52eeaacf67e3dd3e0beb9
SparksFlash <ug2102056@cse.pstu.ac.bd>
2026-03-07 16:50:35 +0600

    diagonalSum optimized

--- 2D-Arrays/diagonalSum.cpp
+++ 2D-Arrays/diagonalSum.cpp
@@ -1,19 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
- int diagonalSum(vector<vector<int>> &mat, int n){
+// //O(n^2)
+// int diagonalSum(vector<vector<int>> &mat, int n){
+//     int sum = 0;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             if(i == j)
+//                 sum += mat[i][j];
+//             else if(j == n-i-1)
+//                 sum += mat[i][j];
+//         }
+//     }
+//     cout << sum << '\n';
+//     return sum;
+// }
+
+//O(n) Optimized
+int diagonalSum(vector<vector<int>> &mat, int n){
     int sum = 0;
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(i == j)
-                sum += mat[i][j];
-            else if(j == n-i-1)
-                sum += mat[i][j];
-        }
+        sum += mat[i][i];
+        if(i != n-i-1)
+           sum += mat[i][n-i-1];
     }
     cout << sum << '\n';
     return sum;
- }
+}
 
 int main(){
     int n;

6df353351f6e614218342f63985300582b61fdea
SparksFlash <ug2102056@cse.pstu.ac.bd>
2026-03-07 14:06:05 +0600

    spiralMatrix

--- 2D-Arrays/spiralMatrix.cpp
+++ 2D-Arrays/spiralMatrix.cpp
@@ -1,20 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void spiralMatrix(int arr[][4], int n, int m){
-    //top
+void spiralMatrix(vector<vector<int>> &arr, int n, int m)
+{
+    int srow = 0, scol = 0;
+    int erow = n - 1, ecol = m - 1;
 
-    //right
+        while(srow <= erow && scol <= ecol){
+            for(int j=scol; j<=ecol; j++){
+                cout << arr[srow][j] << " ";
+            }
 
-    //bottom
+            for(int i=srow+1; i<=erow; i++){
+                cout << arr[i][ecol] << " ";
+            }
 
-    //left
+            for(int j=ecol-1; j>=scol; j--){
+                if(srow == erow)
+                   break;
+                cout << arr[erow][j] << " ";
+            }
+
+            for(int i=erow-1; i>=srow+1; i--){
+                if(scol == ecol)
+                   break;
+                cout << arr[i][scol] << " ";
+            }
+            srow++, scol++;
+            erow--, ecol--;
+        }
+
+    cout << endl;
 }
 
-int main(){
-    int arr[4][4] = {{1, 2, 3, 4},
-                     {5, 6, 7, 8},
-                     {9, 10, 11, 12},
-                     {13, 14, 15, 16}};
-    
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> arr(n, vector<int>(m));
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> arr[i][j];
+
+    spiralMatrix(arr, n, m);
 }

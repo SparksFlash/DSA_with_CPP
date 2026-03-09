f0a09bede364429bde83a7be9b8be55a9780d3d2
SparksFlash <ug2102056@cse.pstu.ac.bd>
2026-03-09 23:44:13 +0600

    searcInaSortedMatrix leetcode optimized solve

--- 2D-Arrays/searchInaSortedMatrix.cpp
+++ 2D-Arrays/searchInaSortedMatrix.cpp
@@ -3,7 +3,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool search(int mat[][4], int n, int m, int key){
+    int row = 0, col = m-1;
+    while(row < n && col >= 0){
+        if(mat[row][col] == key){
+            cout << "Found at cell (" << row << "," << col <<")\n";
+            return true;
+        }else if(mat[row][col] > key){
+            col--;
+        }else{
+            row++;
+        }
+    }
 
+    cout << "Key not Found\n";
+    return false;
+}
 
 int main(){
+    int matrix[4][4] = {{10, 20, 30, 40},
+                        {15, 25, 35, 45},
+                        {27, 29, 37, 48},
+                        {32, 33, 39, 50}};
+
+    search(matrix, 4, 4, 33);
 }
